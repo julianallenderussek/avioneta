0b58268d21cf0bfa04af267fe84ab9e21d9a0937
jullsrussek <jallende@bu.edu>
2021-03-08 18:47:23 -0600

    Second commit

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-const { nearer } = require("q")
+
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
+  console.log('click')
 })
 
 //Send Email function
-function sendEmail(name, last_name, phone, mail, razon_visita) {
-  console.log('email is being send')
+function sendEmail(name, last_name, phone, user_email, razon_visita) {
   Email.send({
     Host: "smtp.gmail.com",
-    Username: "julianallenderussek@gmail.com",
-    Password: "libtfbzsqgzindyb",
-    To: `${mail}`,
-    From: "julianallenderussek@gmail.com",
-    Subject: `${name} gracias por contactar avioneta`,
-    Body: `Name:${name}, muchas gracias por contactarnos. Uno de nuestros agentes se pondra en contacto contigo. Gracias por dejarnos tus datos, por favor verifica si son correctos. Sino nos puedes escribir al siguiente numéro. 8124486070 
-    <br/>
+    Username: "ffuentesdelaparra@gmail.com",
+    Password: "mschbavemdoqhfng",
+    // To: (`${user_email}`, 'jallenderussek@gmail.com', 'ffuentesdelaparra@gmail.com'),
+    To: `${user_email}`,
+    From: "contacto@avioneta.org",
+    Subject: `Avioneta Contacto: ${name} gracias por ponerte en contacto`,
+    Body: `Estimado: ${name}:<br/>
+     Muchas gracias por contactarnos. <br/>
+     Uno de nuestros agentes se pondra en contacto contigo enseguida. <br/>Gracias por dejarnos tus datos, por favor verifica si son correctos.<br/> 
+     Sino nos puedes escribir al siguiente numéro. 8124486070 <br/>
+     <br/>
+    
     Nombre: ${name}
     <br/>
     Apellido: ${last_name}
     <br/>
-    Email: ${mail}
+    Email: ${user_email}
     <br/>
     Razón Vista: ${razon_visita}
     <br/>
@@ -32,6 +37,7 @@ function sendEmail(name, last_name, phone, mail, razon_visita) {
   }).then(
     message => alert('Gracias por contactarnos, un correo fue enviado a tu mail')
   );
+  console.log(`${user_email}`, 'jallenderussek@gmail.com', 'ffuentesdelaparra@gmail.com')
 }
 
 
@@ -39,7 +45,7 @@ function sendEmail(name, last_name, phone, mail, razon_visita) {
 function validation() {
   var name = document.getElementById('name').value
   var last_name = document.getElementById('last_name').value
-  var mail = document.getElementById('email').value
+  var user_email = document.getElementById('user_email').value
   var phone = document.getElementById('phone').value
   var razon_visita = document.getElementById('razon_visita').value
   var error_message = document.getElementById('error_message');
@@ -63,13 +69,13 @@ function validation() {
     return false
   }
 
-  if (mail.length < 1) {
+  if (user_email.length < 1) {
     text = "Favor de ingresar un email"
     error_message.innerHTML = text
     return false
   }
-    alert('Form Submitted Succesfully')
-    sendEmail(name, last_name, phone, mail, razon_visita)
-  
+    console.log(user_email)
+    sendEmail(name, last_name, phone, user_email, razon_visita)
+    alert('Gracias por contactarnos. Un correo electronico fue enviado a tu mail')
     return true
 }
